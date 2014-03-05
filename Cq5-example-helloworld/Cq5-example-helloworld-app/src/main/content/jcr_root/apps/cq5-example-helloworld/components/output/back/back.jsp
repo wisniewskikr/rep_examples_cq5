1debc957a6606297a34d2e880919bbcf233653a0
unknown <wisniewk@POZMWCOE27631.emea.roche.com>
2014-03-05 09:09:04 +0100

    Add annotation PostConstruct in project Cq5-example-helloworld

--- Cq5-example-helloworld/Cq5-example-helloworld-app/src/main/content/jcr_root/apps/cq5-example-helloworld/components/output/back/back.jsp
+++ Cq5-example-helloworld/Cq5-example-helloworld-app/src/main/content/jcr_root/apps/cq5-example-helloworld/components/output/back/back.jsp
@@ -3,15 +3,12 @@
 <%@taglib prefix="sling2" uri="http://sling.apache.org/taglibs/sling" %>
 <sling2:adaptTo adaptable="${resource}" adaptTo="pl.kwi.model.output.OutputBackModel" var="model"/>
 
-<c:set var="backTarget" value="<%= resourceResolver.map(properties.get("backTarget", currentPage.getPath())) %>"/>
-<c:set var="currentPath" value="<%= currentPage.getPath() %>"/>
-
 
 
 <div class="buttons">
-	<input type="button" onclick="goTo('${backTarget}');" id="back" name="back" value="${model.textBack}" title="${model.backHint}"/>
+	<input type="button" onclick="goTo('${model.backTarget}');" id="back" name="back" value="${model.textBack}" title="${model.backHint}"/>
 	
-	<c:if test="${backTarget == currentPath}">
+	<c:if test="${model.backTargetEmpty}">
 		<div class="errorMessages">
 			Please fill mandatory field: Edit -> Back button -> Button 'Back' target 
 		</div>
--- Cq5-example-helloworld/Cq5-example-helloworld-bundle/src/main/java/pl/kwi/model/output/OutputBackModel.java
+++ Cq5-example-helloworld/Cq5-example-helloworld-bundle/src/main/java/pl/kwi/model/output/OutputBackModel.java
@@ -1,9 +1,15 @@
 package pl.kwi.model.output;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
 
 @Model(adaptables=Resource.class)
 public class OutputBackModel {
@@ -14,7 +20,39 @@ public class OutputBackModel {
 	
 	@Inject
 	private String backHint;
+	
+	@Inject @Default
+	private String backTarget;
+	
+	private boolean backTargetEmpty;
+	
+	private Resource resource;
+	
+	private ResourceResolver resourceResolver;
+	
+	
+	public OutputBackModel(Resource resource) {
+		this.resource = resource;
+		this.resourceResolver = resource.getResourceResolver();
+	}
 
+	
+	@PostConstruct
+	public void init() {
+		
+		backTargetEmpty = false;
+		
+		if("".equals(backTarget)) {
+			backTargetEmpty = true;
+			PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+			Page page = pageManager.getContainingPage(resource);
+			backTarget = page.getPath();
+		}
+		
+		backTarget = resourceResolver.map(backTarget);		
+		
+	}
+	
 	
 	public String getTextBack() {
 		return textBack;
@@ -28,7 +66,21 @@ public class OutputBackModel {
 	}
 	public void setBackHint(String backHint) {
 		this.backHint = backHint;
+	}	
+
+	public String getBackTarget() {
+		return backTarget;
 	}
+	public void setBackTarget(String backTarget) {
+		this.backTarget = backTarget;
+	}
+
+	public boolean isBackTargetEmpty() {		
+		return backTargetEmpty;
+	}
+	public void setBackTargetEmpty(boolean backTargetEmpty) {
+		this.backTargetEmpty = backTargetEmpty;
+	}	
 	
 	
 }
