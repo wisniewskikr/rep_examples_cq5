0f7241b2f637c7de3b77e4b74016614a91362fb7
unknown <wisniewk@POZMWCOE27631.emea.roche.com>
2014-03-06 14:56:52 +0100

    Improvement of content of input in project Cq5-example-helloworld

--- Cq5-example-helloworld/Cq5-example-helloworld-app/src/main/content/jcr_root/apps/cq5-example-helloworld/components/input/form/content/content.jsp
+++ Cq5-example-helloworld/Cq5-example-helloworld-app/src/main/content/jcr_root/apps/cq5-example-helloworld/components/input/form/content/content.jsp
@@ -1,21 +1,18 @@
 <%@include file="/libs/foundation/global.jsp" %>
 <%%>
 <%@taglib prefix="sling2" uri="http://sling.apache.org/taglibs/sling" %>
-<sling2:adaptTo adaptable="${resource}" adaptTo="pl.kwi.model.input.form.InputContentModel" var="model"/>
-
-<c:set var="currentPagePath" value="<%= currentPage.getPath() %>"/>
-<c:set var="errorField" value="<%= request.getParameter("errorField") %>"/>
+<sling2:adaptTo adaptable="${slingRequest}" adaptTo="pl.kwi.model.input.form.InputContentModel" var="model"/>
 
 
 
 <div class="content">
 
-	<input type="hidden" id="currentPagePath" name="currentPagePath" value="${currentPagePath}"/>
+	<input type="hidden" id="currentPagePath" name="currentPagePath" value="${model.currentPagePath}"/>
 
 	<div class="contentElementInput">
 		<div class="text">
 			${model.textName} * 
-			<c:if test="${errorField == 'name'}"><span class="error" id="nameErrorMessage">${model.nameErrorMessage}</span></c:if>
+			<c:if test="${model.errorField == 'name'}"><span class="error" id="nameErrorMessage">${model.nameErrorMessage}</span></c:if>
 		</div>
 		<div class="input"><input type="text" id="name" name="name"/></div>
 		<div class="description" id="nameDescription">${model.nameDescription}</div>
--- Cq5-example-helloworld/Cq5-example-helloworld-bundle/src/main/java/pl/kwi/model/input/form/InputContentModel.java
+++ Cq5-example-helloworld/Cq5-example-helloworld-bundle/src/main/java/pl/kwi/model/input/form/InputContentModel.java
@@ -1,22 +1,55 @@
 package pl.kwi.model.input.form;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Via;
 
-@Model(adaptables=Resource.class)
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+
+@Model(adaptables=SlingHttpServletRequest.class)
 public class InputContentModel {
 	
 	
-	@Inject
+	@Inject @Via("resource")
 	private String textName;
 
-	@Inject
+	@Inject @Via("resource")
 	private String nameErrorMessage;
 	
-	@Inject
+	@Inject @Via("resource")
 	private String nameDescription;
+	
+	private String currentPagePath;
+	
+	private String errorField;
+	
+	private SlingHttpServletRequest request;
+	
+	private Resource resource;
+	
+	private ResourceResolver resourceResolver;
+	
+	
+	public InputContentModel(SlingHttpServletRequest request) {
+		this.request = request;
+		this.resource = request.getResource();
+		this.resourceResolver = resource.getResourceResolver();
+	}
+	
+	
+	@PostConstruct
+	public void init() {
+		PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+		Page page = pageManager.getContainingPage(resource);
+		currentPagePath = page.getPath();
+		errorField = request.getParameter("errorField");
+	}
 
 	
 	public String getTextName() {
@@ -38,7 +71,21 @@ public class InputContentModel {
 	}
 	public void setNameDescription(String nameDescription) {
 		this.nameDescription = nameDescription;
+	}
+
+	public String getCurrentPagePath() {
+		return currentPagePath;
+	}
+	public void setCurrentPagePath(String currentPagePath) {
+		this.currentPagePath = currentPagePath;
+	}
+
+	public String getErrorField() {
+		return errorField;
+	}
+	public void setErrorField(String errorField) {
+		this.errorField = errorField;
 	}	
-	
+		
 		
 }
